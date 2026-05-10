d9fecf17c3647f393f9749902fc8824546844b22
Md Sahrior Haider <165468630+Sahrior@users.noreply.github.com>
2026-05-11 02:31:16 +0600

    3 hour 15 minutes

---
+++ src/components/CryptoCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CryptoCard = ( {crypto} ) => {
+  return (
+    <div className='crypto-card' >
+      
+    </div>
+  )
+}
+
+export default CryptoCard
---
+++ src/pages/CoinDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CoinDetail = () => {
+  return (
+    <div>
+      coindetails
+    </div>
+  )
+}
+
+export default CoinDetail
